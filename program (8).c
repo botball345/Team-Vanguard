d6d23b72f32479c312936ffd851f89d7fea6bea3
botball345 <102452108+botball345@users.noreply.github.com>
2023-02-15 16:40:24 -0800

    Update program (8).c

--- program (8).c
+++ program (8).c
@@ -1,35 +1,18 @@
-#include <stdio.h>
-#include <kipr/wombat.h>
+ #include <stdio.h>
+ #include <kipr/wombat.h>
 
-int main()
-{
- 
- while(1==1)
-{
-    if(analog(1)>3000)//on black 
-    {
-      motor(0,0);
-      motor(3,100);
+ int main() {
+   while (1 == 1) {
+     if (analog(1) > 3000) //on black 
+     {
+       motor(0, 0);
+       motor(3, 100);
 
-    }
-    else// on white 
-    {
-      motor(0,100);
-      motor(3,0);
-      
-
-    }
-     
-    
-    
-
-
-
-
-  }
- 
- 
- 
- 
-  return 0;
-}
+     } else // on white 
+     {
+       motor(0, 100);
+       motor(3, 0);
+     }
+   }
+   return 0;
+ }
