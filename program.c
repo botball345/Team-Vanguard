c7e51d9a3480861efee6c16176932756083159fe
botball345 <102452108+botball345@users.noreply.github.com>
2023-02-22 16:37:44 -0800

    Added Debugging

--- program.c
+++ program.c
@@ -5,20 +5,28 @@ int left = 0;
 int right = 3;
 void drive (int leftvelocity, int rightvelocity)
 {
-motor (left, leftvelocity);
-motor (right, rightvelocity);
+  motor (left, leftvelocity);
+  motor (right, rightvelocity);
 }
 int main()
 {
   while (1==1)
   {
+    printf("%d\n", analog(1));
     if (analog(1)>2000)
-    {
-      drive (100,0);
-    }
-    else 
     {
       drive (0,100);
     }
+    else
+    {
+      drive (100,0);
+    }
   }
+  
 }
+
+
+
+
+
+
